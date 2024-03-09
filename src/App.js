ef9666571fdb5ba99b86d22667dc06336eb4144d
yellasai <yellasaisomasekhar@lyrostech.com>
2024-03-09 11:14:38 +0530

    box modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header'
+import Footer from './Footer'
+import Box1 from './Box1'
+import Box2 from './Box2'
 function App() {
   return (
-  <div><h1>hello world</h1></div>
+    <div className='body1'>
+      <div className='header'><Header /></div>
+      <div className='container'>
+        <div className='b1'><Box1 /></div>
+        <div className='b2'><Box2 /></div>
+      </div>
+      <div className='footer'><Footer /></div>
+    </div> 
   );
 }
-
 export default App;
